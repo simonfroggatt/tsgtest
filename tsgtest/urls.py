d8317f7242450a021df01a711a1cae40f8bb93ab
simonfroggatt <simon@safetysignsandnotices.co.uk>
2024-11-18 13:38:43 +0400

    media testing

--- tsgtest/urls.py
+++ tsgtest/urls.py
@@ -18,11 +18,15 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import path, include,re_path
 
 urlpatterns = [
     path('', include("basic.urls")),
     path("basic/", include("basic.urls")),
     path('admin/', admin.site.urls),
+
+re_path(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
